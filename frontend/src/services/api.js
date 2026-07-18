64fd7572c48f7b76e288f52f776faca82fccae7c
subhrank09 <167698982+subhrank09@users.noreply.github.com>
2026-07-18 22:28:56 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,18 +1,9 @@
 import axios from "axios";
 
 console.log("API URL:", import.meta.env.VITE_API_URL);
-<<<<<<< HEAD
 
 const API = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 });
 
 export default API;
-=======
-
-const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
-});
-
-export default API;
->>>>>>> bf3c5fa (connection)
