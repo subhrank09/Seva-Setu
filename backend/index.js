0601b70ee577466e884f47afaa268be30cabf069
subhrank09 <167698982+subhrank09@users.noreply.github.com>
2026-07-18 22:13:19 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -42,6 +42,8 @@ app.get("/api/protected", protect, (req, res) => {
   });
 });
 app.use("/api/complaints", complaintRoutes);
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`🚀 Server running on port ${PORT}`);
 });
