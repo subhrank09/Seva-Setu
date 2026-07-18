ad3f1155e2e40279109f7cb62660b6612cae3de3
subhrank09 <167698982+subhrank09@users.noreply.github.com>
2026-07-18 22:45:58 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -4,14 +4,4 @@ const API = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 });
 
-API.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-
-  return config;
-});
-
 export default API;
