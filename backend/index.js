9a2353702fa9aeba2543aa71c1b95f66c08cefae
subhrank09 <167698982+subhrank09@users.noreply.github.com>
2026-07-18 22:13:43 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -19,11 +19,8 @@ async function testDB() {
 testDB();
 const app = express();
 app.use(cors({
-  origin: [
-    "http://localhost:5173",
-    "https://YOUR_PROJECT.vercel.app"
-  ],
-  credentials: true
+  origin: process.env.CLIENT_URL || "http://localhost:5173",
+  credentials: true,
 }));
 app.use(express.json());
 
